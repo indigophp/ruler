0e67367f7c952b10b2e595df604a3335bb374677
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-10-29 00:37:13 +0100

    Adds target value test

---
+++ spec/Indigo/Ruler/Stub/TargetValueSpec.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace spec\Indigo\Ruler\Stub;
+
+use PhpSpec\ObjectBehavior;
+
+class TargetValueSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Indigo\Ruler\Stub\TargetValue');
+        $this->shouldUseTrait('Indigo\Ruler\TargetValue');
+    }
+
+    function it_should_allow_to_construct_with_value()
+    {
+        $this->beConstructedWith('target');
+
+        $this->getTargetValue()->shouldReturn('target');
+    }
+
+    function it_should_allow_to_set_target()
+    {
+        $this->setTargetValue('target');
+
+        $this->getTargetValue()->shouldReturn('target');
+    }
+
+    function getMatchers()
+    {
+        return [
+            'useTrait' => function ($subject, $trait) {
+                return class_uses($subject, $trait);
+            }
+        ];
+    }
+}
---
+++ stub/TargetValue.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Indigo Ruler package.
+ *
+ * (c) Indigo Development Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Indigo\Ruler\Stub;
+
+/**
+ * Target Value Stub
+ *
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ */
+class TargetValue
+{
+    use \Indigo\Ruler\TargetValue;
+}
