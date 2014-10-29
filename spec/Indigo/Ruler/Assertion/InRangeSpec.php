0de4eb75cbdf5046a9d130035218e1d211a92aa7
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-10-29 03:35:51 +0100

    Adds InRange assertion spec

---
+++ spec/Indigo/Ruler/Assertion/InRangeSpec.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace spec\Indigo\Ruler\Assertion;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class InRangeSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith([1, 5]);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Indigo\Ruler\Assertion\InRange');
+        $this->shouldHaveType('Indigo\Ruler\Assertion');
+    }
+
+    public function it_should_allow_to_be_in_range()
+    {
+        $this->assert(3)->shouldReturn(true);
+    }
+
+    public function it_should_not_allow_to_be_out_of_range()
+    {
+        $this->assert(0)->shouldReturn(false);
+    }
+}
